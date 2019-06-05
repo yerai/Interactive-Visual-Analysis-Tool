3d5c276e74ec2072b8308ccdc15d76bc5af17dc4
Yerai <yeerizg@gmail.com>
2019-06-05 02:46:47 +0200

    Added Scatter Plot

--- app.R
+++ app.R
@@ -1,10 +1,7 @@
 # Load packages ----
 library(shiny)
-
-#
 library(ggplot2)
-#
-
+library(plotly)
 
 # Load data ----
 movies <- readRDS("data/movies.rds")
@@ -12,6 +9,9 @@ movies <- readRDS("data/movies.rds")
 # Global variables ----
 year_list <- sort(unique(movies$year))
 genres_list = colnames(movies)[18:24]
+budget_list <- sort(unique(movies$budget))
+budget_list <- budget_list[2:length(budget_list)]
+rating_list <- sort(unique(movies$rating))
 
 # Define UI  ----
 ui <- fluidPage(
@@ -51,13 +51,13 @@ ui <- fluidPage(
   
   navbarPage("Movie Interactive Visual Analysis Tool",
     tabPanel(
-       title ="Genre Explorer",
+       title ="Genre Trends",
        plotOutput("graph"),
        hr(),
        fluidRow(
          column(
            width = 12, 
-           h3("Movie genre explorer")
+           h3("Controls")
          ),
          column(
            width = 6,
@@ -94,7 +94,56 @@ ui <- fluidPage(
              
     ),#TabPanel 1
     tabPanel(
-      title ="Best Movies"
+      title ="Movies",
+      plotlyOutput("graph2"),
+      hr(),
+      fluidRow(
+        column(
+          width = 12, 
+          h3("Controls")
+        ),
+        column(
+          width = 6,
+          sliderInput(
+            "budget_range", 
+            label = h4("Budget range"), 
+            min = budget_list[1], 
+            max = budget_list[length(budget_list)], 
+            value = c(budget_list[1], budget_list[length(budget_list)]),
+            width = '100%')
+        ),
+        column(
+          width = 6,
+          sliderInput(
+            "rating_range", 
+            label = h4("Rating range"), 
+            min = rating_list[1], 
+            max = rating_list[length(rating_list)], 
+            value = c(rating_list[1], rating_list[length(rating_list)]),
+            width = '100%')
+        ),
+        column(
+          width = 4,
+          tags$div(align = 'left',
+                   class = 'multicol',
+                   checkboxGroupInput(inputId  = 'decades_selected',
+                                      label    = h4("Decades displayed"), 
+                                      choices  = c("1910","1920","1930","1940","1950","1960","1970","1980","1990","2000"),
+                                      selected = c("1910","1920","1930","1940","1950","1960","1970","1980","1990","2000"),
+                                      width = '100%',
+                                      inline   = TRUE)
+          )
+        ),
+        column(
+          width = 2,
+          radioButtons("color_palette_scatter", 
+                       label = h4("Color palette"), 
+                       choices = c("Light", "Dark"), 
+                       selected = "Dark",
+                       inline = FALSE, 
+                       width = '100%')
+        )
+      )#FluidRow
     )#TabPanel 2
   )#Navbar
 )#UI
@@ -105,13 +154,6 @@ ui <- fluidPage(
 
 # Define server logic ----
 server <- function(input, output) {
-  
-  
-  output$graph <- renderPlot({
-    
-    
-    
-  })
   
   output$graph <- renderPlot({
     
@@ -154,6 +196,7 @@ server <- function(input, output) {
     
     t_lista <- t(lista)
     
+    
     # Plot graph ---
     xrange <- range(filtered_year_list)
     yrange <- c(0,max(as.numeric(unlist(t_lista))))
@@ -168,11 +211,72 @@ server <- function(input, output) {
     
     genre_count = 1
     for (genre in filtered_genres_list){
-      lines(filtered_year_list, t_lista[genre_count,], pch=19, col=colors[genre_count], type="b", lty=1)
+      lines(filtered_year_list, t_lista[genre_count,], pch=19, col=colors[genre_count], type="l", lty=1, lwd=4)
     genre_count = genre_count + 1
     }
-    legend("topleft", legend=filtered_genres_list, col=colors, lty=1, cex=0.8)
+    legend("topleft", legend=filtered_genres_list, col=colors, lty=1, cex=0.8, lwd=6)
   })
+  
+  output$graph2 <- renderPlotly({
+    budget_list <- c()
+    title_list <- c()
+    rating_list <- c()
+    decade_list <- c()
+    
+    counter = 1;
+    for(budget in movies$budget){
+      if(!(budget == 0) && !is.na(budget)){
+        if(budget >= input$budget_range[1] && budget <= input$budget_range[2]
+            && movies$rating[counter] >= input$rating_range[1] && movies$rating[counter] <= input$rating_range[2]){
+          decade <- paste(substr(as.character(movies$year[counter]),1,3), sep="", 0)
+          if(decade %in% input$decades_selected){
+            budget_list <- c(budget_list, budget)
+            title_list <- c(title_list, movies$title[counter])
+            rating_list <- c(rating_list, movies$rating[counter])
+            decade_list <- c(decade_list, paste(substr(as.character(movies$year[counter]),1,3), sep="", 0))
+          }
+        }
+      }
+      counter = counter + 1
+    }
+    
+    # Get color selected ---
+    colors <- c()
+    if (input$color_palette_scatter == "Light"){
+      colors <-  c('#8dd3c7','#ffffb3','#bebada','#fb8072','#80b1d3','#fdb462','#b3de69','#fccde5','#d9d9d9','#bc80bd')
+    }else{
+      colors <- c('#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#e31a1c','#fdbf6f','#ff7f00','#cab2d6','#6a3d9a')
+    }
+    
+    p <- plot_ly(x = budget_list, 
+                 y = rating_list,
+                 color = decade_list,
+                 colors = colors,
+                 type = "scatter",
+                 mode = "markers",
+                 hoverinfo = "text",
+                 hovertext = paste(title_list)
+                 
+                 #marker = list(size = 10,
+                               #color = 'rgba(255, 182, 193, .9)',
+                               #line = list(color = 'rgba(152, 0, 0, .8)',
+                                #           width = 2))
+                 )%>% 
+          layout(title = "Plot Title",
+                 xaxis = list(title = "Budget"),
+                 yaxis = list(title = "Rating")) %>% 
+          config(displayModeBar = F)
+    
+   
+    
+    #plot(budget_list, rating_list, main="Scatterplot Example", 
+    #     xlab="budget", ylab="rating", pch=19)
+    
+    #text(budget_list, rating_list, labels=title_list, cex= 0.7, pos=3)
+    
+    
+  })
+  
 }
 
 # Run the app ----
